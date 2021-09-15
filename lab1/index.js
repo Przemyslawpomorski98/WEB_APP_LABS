283ac69d2e238706deeb1df8b7627d57d019d0c8
Przemyslaw Pomorski <pomorskip2@gmail.com>
2021-09-15 21:02:10 +0200

    lab1 done

--- lab1/index.js
+++ lab1/index.js
@@ -1,69 +1,117 @@
 var App = /** @class */ (function () {
     function App() {
+        this.inputValues = [];
+        this.countNumber = 1;
         this.start();
     }
     App.prototype.start = function () {
         this.getInputs();
-        this.checkValues();
+        this.checkGenerationValues();
     };
     App.prototype.getInputs = function () {
         this.generateInput = document.querySelector('#generateInput');
-        this.userInputs = document.querySelector('#userInputs');
-        // this.input1 = document.querySelector('#input1');
-        // this.input2 = document.querySelector('#input2');
-        // this.input3 = document.querySelector('#input3');
-        // this.input4 = document.querySelector('#input4');
+        this.generateInputsWrapper = document.querySelector('#generateInputs');
+        this.userInputsWrapper = document.querySelector('#userInputs');
+        this.answerInputs = document.querySelector('#answerInputs');
         this.sum_input = document.querySelector('#sum');
         this.avg_input = document.querySelector('#avg');
         this.min_input = document.querySelector('#min');
         this.max_input = document.querySelector('#max');
     };
-    App.prototype.checkValues = function () {
+    App.prototype.checkGenerationValues = function () {
         var _this = this;
-        this.generateInput.addEventListener('input', function () { return _this.generateInputs(); });
-        // this.input1.addEventListener('input', () => this.getCalculatedValues());
-        // this.input2.addEventListener('input', () => this.getCalculatedValues());
-        // this.input3.addEventListener('input', () => this.getCalculatedValues());
-        // this.input4.addEventListener('input', () => this.getCalculatedValues());
+        this.generateInput.addEventListener('input', function (e) {
+            var target = e.target;
+            _this.countNumber = Number(target.value);
+            _this.pushCalculations(_this.countNumber, _this.inputValues);
+        });
+        this.pushCalculations(this.countNumber, this.inputValues);
     };
-    App.prototype.generateInputs = function () {
-        var generateValue = +this.generateInput.value;
-        while (this.userInputs.hasChildNodes()) {
-            this.userInputs.removeChild(this.userInputs.lastChild);
+    App.prototype.pushCalculations = function (inputCount, values) {
+        var calc = new Calculations;
+        var sections = [];
+        this.generateNumberInputs(inputCount, values);
+        var countValues = values.slice(0, inputCount);
+        sections.push(this.generateStat(this.sum_input, calc.sum, countValues));
+        sections.push(this.generateStat(this.avg_input, calc.average, countValues));
+        sections.push(this.generateStat(this.min_input, calc.min, countValues));
+        sections.push(this.generateStat(this.max_input, calc.max, countValues));
+    };
+    App.prototype.generateNumberInputs = function (count, values) {
+        while (this.userInputsWrapper.hasChildNodes()) {
+            this.userInputsWrapper.removeChild(this.userInputsWrapper.lastChild);
         }
-        for (var i = 1; i <= generateValue; i++) {
-            var generatedInput = document.createElement('input');
-            generatedInput.type = 'text';
-            generatedInput.id = 'input' + i;
-            this.userInputs.appendChild(generatedInput);
+        for (var i = 0; i < count; i++) {
+            var input = new InputDetails(i, count, values).render();
+            this.userInputsWrapper.appendChild(input);
         }
-        this.getGeneratedInputs(generateValue);
     };
-    App.prototype.getGeneratedInputs = function (generateValue) {
-        var _this = this;
-        for (var i = 0; i <= generateValue; i++) {
-            this.newInput = document.querySelector('#input' + i);
-        }
-        this.newInput.addEventListener('input', function () { return _this.getCalculatedValues(); });
-    };
-    App.prototype.getCalculatedValues = function () {
-        var value1 = +this.input1.value;
-        var value2 = +this.input2.value;
-        var value3 = +this.input3.value;
-        var value4 = +this.input4.value;
-        var sum = value1 + value2 + value3 + value4;
-        var avg = sum / 4;
-        var min = Math.min(value1, value2, value3, value4);
-        var max = Math.max(value1, value2, value3, value4);
-        this.showResults(sum, avg, min, max);
-    };
-    App.prototype.showResults = function (sum, avg, min, max) {
-        this.sum_input.value = sum.toString();
-        this.avg_input.value = avg.toString();
-        this.min_input.value = min.toString();
-        this.max_input.value = max.toString();
+    App.prototype.generateStat = function (inputType, calculations, values) {
+        inputType.value = calculations(values);
+        return inputType.value;
     };
     return App;
 }());
-;
+var InputDetails = /** @class */ (function () {
+    function InputDetails(id, count, values) {
+        this.start(id, count, values);
+    }
+    InputDetails.prototype.start = function (id, count, values) {
+        this.inputCreation(id, count, values);
+        this.buttonCreation(id, count, values);
+    };
+    InputDetails.prototype.inputCreation = function (id, count, values) {
+        this.userInput = document.createElement('input');
+        this.userInput.type = "text";
+        this.userInput.value = values[id] ? String(values[id]) : '0';
+        this.userInput.id = "input-" + id;
+        values[id] = Number(this.userInput.value);
+        this.userInput.addEventListener('input', function (e) {
+            var target = e.target;
+            values[id] = Number(target.value);
+            new App().pushCalculations(count, values);
+        });
+    };
+    InputDetails.prototype.buttonCreation = function (id, count, values) {
+        var _this = this;
+        this.inputDeleteBtn = document.createElement('button');
+        this.inputDeleteBtn.innerText = "Delete";
+        this.inputDeleteBtn.addEventListener('click', function (e) {
+            _this.generateInput = document.getElementById('generateInput');
+            console.log(values);
+            values[id] = 0;
+            values.splice(id, 1);
+            count -= 1;
+            _this.generateInput.value = count;
+            console.log({ count: count });
+            new App().pushCalculations(count, values);
+        });
+    };
+    InputDetails.prototype.render = function () {
+        var container = document.createElement('div');
+        container.appendChild(this.userInput);
+        container.appendChild(this.inputDeleteBtn);
+        return container;
+    };
+    return InputDetails;
+}());
+var Calculations = /** @class */ (function () {
+    function Calculations() {
+    }
+    Calculations.prototype.sum = function (values) {
+        var sum = values.reduce(function (a, b) { return a + b; }, 0);
+        return sum;
+    };
+    Calculations.prototype.average = function (values) {
+        var sum = values.reduce(function (a, b) { return a + b; }, 0);
+        return Number((sum / values.length).toFixed(2));
+    };
+    Calculations.prototype.min = function (values) {
+        return Math.min.apply(Math, values);
+    };
+    Calculations.prototype.max = function (values) {
+        return Math.max.apply(Math, values);
+    };
+    return Calculations;
+}());
 var app = new App();
--- lab1/index.ts
+++ lab1/index.ts
@@ -1,19 +1,16 @@
-class App{
+class App {
     generateInput: HTMLInputElement;
-
-    userInputs: HTMLInputElement;
-
-    newInput: HTMLInputElement;
-
-    // input1: HTMLInputElement;
-    // input2: HTMLInputElement;
-    // input3: HTMLInputElement;
-    // input4: HTMLInputElement;
+    generateInputsWrapper: HTMLInputElement;
+    userInputsWrapper: HTMLInputElement;
+    answerInputs: HTMLInputElement;
 
     sum_input: HTMLInputElement;
     avg_input: HTMLInputElement;
     min_input: HTMLInputElement;
     max_input: HTMLInputElement;
+
+    inputValues: Array<number> = [];
+    countNumber: number = 1;
 
     constructor(){
         this.start();
@@ -21,75 +18,143 @@ class App{
 
     start(){
         this.getInputs();
-        this.checkValues();
+        this.checkGenerationValues();
     }
 
     getInputs(){
         this.generateInput = document.querySelector('#generateInput');
-        this.userInputs = document.querySelector('#userInputs');
-        // this.input1 = document.querySelector('#input1');
-        // this.input2 = document.querySelector('#input2');
-        // this.input3 = document.querySelector('#input3');
-        // this.input4 = document.querySelector('#input4');
+        this.generateInputsWrapper = document.querySelector('#generateInputs');
+        this.userInputsWrapper = document.querySelector('#userInputs');
+        this.answerInputs = document.querySelector('#answerInputs');
         this.sum_input = document.querySelector('#sum');
         this.avg_input = document.querySelector('#avg');
         this.min_input = document.querySelector('#min');
         this.max_input = document.querySelector('#max');
     }
 
-    checkValues(){
-        this.generateInput.addEventListener('input', () => this.generateInputs());
-        
-        // this.input1.addEventListener('input', () => this.getCalculatedValues());
-        // this.input2.addEventListener('input', () => this.getCalculatedValues());
-        // this.input3.addEventListener('input', () => this.getCalculatedValues());
-        // this.input4.addEventListener('input', () => this.getCalculatedValues());
+    checkGenerationValues() {
+        this.generateInput.addEventListener('input', (e: Event) => {
+
+            const target = e.target as HTMLInputElement;
+            this.countNumber = Number(target.value);
+            this.pushCalculations(this.countNumber, this.inputValues);
+
+        });
+
+        this.pushCalculations(this.countNumber, this.inputValues);
     }
 
-    generateInputs(){
-        const generateValue = +this.generateInput.value;
+    pushCalculations(inputCount: number, values: Array<number>) : void {
 
-        while (this.userInputs.hasChildNodes()){
-            this.userInputs.removeChild(this.userInputs.lastChild);
+        const calc = new Calculations;
+
+        const sections: Array<number> = [];
+
+        this.generateNumberInputs(inputCount, values);
+
+        const countValues = values.slice(0, inputCount);
+
+        sections.push(this.generateStat(this.sum_input, calc.sum, countValues));
+        sections.push(this.generateStat(this.avg_input, calc.average, countValues));
+        sections.push(this.generateStat(this.min_input, calc.min, countValues));
+        sections.push(this.generateStat(this.max_input, calc.max, countValues));
+
+    }
+
+    generateNumberInputs(count: number, values: Array<number>): void{
+        while (this.userInputsWrapper.hasChildNodes()){
+            this.userInputsWrapper.removeChild(this.userInputsWrapper.lastChild);
         }
 
-        for (let i = 1; i <= generateValue; i++){
-            let generatedInput = document.createElement('input');
-            generatedInput.type = 'text';
-            generatedInput.id = 'input' + i;
-            this.userInputs.appendChild(generatedInput);
+        for (let i = 0; i < count; i++) {
+            const input = new InputDetails(i,count, values).render();
+            this.userInputsWrapper.appendChild(input);
         }
-
-        this.getGeneratedInputs(generateValue)
     }
 
-    getGeneratedInputs(generateValue){
-        for (let i = 1; i <= generateValue; i++){
-            this.newInput = document.querySelector('#input' + i);
-        }
-        this.newInput.addEventListener('input', () => this.getCalculatedValues());
+    generateStat(inputType: any, calculations: Function, values: Array<number>) : number {
+
+        inputType.value = calculations(values);
+
+        return inputType.value;
+    }
+}
+
+class InputDetails {
+
+    userInput: HTMLInputElement;
+    inputDeleteBtn: HTMLButtonElement;
+    generateInput: HTMLInputElement;
+
+    constructor(id: number, count: number, values: Array<number>){
+        this.start(id, count, values);
     }
 
-    getCalculatedValues(){
-        const value1 = +this.input1.value;
-        const value2 = +this.input2.value;
-        const value3 = +this.input3.value;
-        const value4 = +this.input4.value;
-
-        const sum = value1 + value2 + value3 + value4;
-        const avg = sum/4;
-        const min = Math.min(value1,value2,value3,value4);
-        const max = Math.max(value1,value2,value3,value4);
-
-        this.showResults(sum,avg,min,max);
+    start(id: number, count: number, values: Array<number>){
+        this.inputCreation(id, count, values);
+        this.buttonCreation(id, count, values);
     }
 
-    showResults(sum: number, avg: number, min:number, max:number){
-        this.sum_input.value = sum.toString();
-        this.avg_input.value = avg.toString();
-        this.min_input.value = min.toString();
-        this.max_input.value = max.toString();
+    inputCreation(id: number, count: number, values: Array<number>){
+        this.userInput = document.createElement('input');
+        this.userInput.type = "text";
+        this.userInput.value = values[id] ? String(values[id]) : '0';
+        this.userInput.id = `input-${id}`;
+        values[id] = Number(this.userInput.value);
+        this.userInput.addEventListener('input', (e: Event) => {
+            const target = e.target as HTMLInputElement;
+            values[id] = Number(target.value);
+            
+            new App().pushCalculations(count, values);
+        });
     }
-};
+
+    buttonCreation(id: number, count: any, values: Array<number>){
+        this.inputDeleteBtn = document.createElement('button');
+        this.inputDeleteBtn.innerText = "Delete";
+
+        this.inputDeleteBtn.addEventListener('click', (e: Event) => {
+
+            this.generateInput = document.getElementById('generateInput') as HTMLInputElement;
+            console.log(values);
+            values[id] = 0;
+            values.splice(id,1);
+            count -= 1;
+            this.generateInput.value = count;
+            console.log({count});
+
+            new App().pushCalculations(count, values);
+        });
+    }
+
+    render() : HTMLDivElement {
+        const container = document.createElement('div');
+        container.appendChild(this.userInput);
+        container.appendChild(this.inputDeleteBtn);
+
+        return container;
+    }
+}
+
+class Calculations {
+
+    sum(values: Array<number>): number {
+        const sum: number = values.reduce((a, b) => a + b, 0);
+        return sum;
+    }
+
+    average(values: Array<number>): number {
+        const sum: number = values.reduce((a, b) => a + b, 0);
+        return Number((sum / values.length).toFixed(2));
+    }
+
+    min(values: Array<number>): number {
+        return Math.min(...values);
+    }
+
+    max(values: Array<number>): number {
+        return Math.max(...values);
+    }
+}
 
 const app = new App();
