d00592dc698ae106530861791a648b9446e7557f
Przemyslaw Pomorski <pomorskip2@gmail.com>
2021-09-17 11:26:38 +0200

    lab4

---
+++ lab4/src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCH5dUBGlYfzb7kWsLGKWMTK4it7UaqFvg",
+  authDomain: "notes-9dac1.firebaseapp.com",
+  projectId: "notes-9dac1",
+  storageBucket: "notes-9dac1.appspot.com",
+  messagingSenderId: "604329667389",
+  appId: "1:604329667389:web:82b35cc5a69145de9af354"
+  };
